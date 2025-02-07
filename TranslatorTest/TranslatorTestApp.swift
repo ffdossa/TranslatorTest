06a8788fda88e1c60f737763c7a099723498441c
ffdossa <127089701+ffdossa@users.noreply.github.com>
2025-02-07 12:25:27 +0200

    Initial Commit

---
+++ TranslatorTest/TranslatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorTestApp.swift
+//  TranslatorTest
+//
+//  Created by Andrii Marchuk on 07.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
